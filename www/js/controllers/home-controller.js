a8437452c7ecc7dc947476314aad5380fd0ad778
rodrigo filomeno <rodrigo.filomeno@al.infnet.edu.br>
2016-12-25 19:14:56 -0200

    mais do mesmo

--- www/js/controllers/home-controller.js
+++ www/js/controllers/home-controller.js
@@ -1,2 +1,16 @@
-angular.module('ReceitasApp')
-    .controller('HomeController', ['$scope', function ($scope) {}]);
+angular.module('ReceitasApp').controller('HomeController', function ($scope) {
+
+        $scope.prato = {
+        titulo : 'Pizza Calabresa',
+        ingredientes : 'Mussarela, calabresa, molho e cebola',
+        preparo : 'Coloque o molho sobre a massa, adicione o queijo e espalhe os outros ingredientes por cima do queijo. Leve ao forno por 10 minutos.',
+        img : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    };
+        $scope.prato2 = {
+        titulo : 'Pizza Mussarela',
+        ingredientes : 'Mussarela e molho',
+        preparo : 'Coloque o molho sobre a massa, adicione o queijo e leve ao forno por 10 minutos.',
+        img : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    };
+
+    });
