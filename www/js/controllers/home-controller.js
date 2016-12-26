0dd348e412be5175d79615742c1630f1547c4eb6
rodrigo filomeno <rodrigo.filomeno@al.infnet.edu.br>
2016-12-26 07:44:07 -0200

    template da lista ok, vou comecar agora o BD

--- www/js/controllers/home-controller.js
+++ www/js/controllers/home-controller.js
@@ -1,16 +1,26 @@
 angular.module('ReceitasApp').controller('HomeController', function ($scope) {
 
-        $scope.prato = {
-        titulo : 'Pizza Calabresa',
-        ingredientes : 'Mussarela, calabresa, molho e cebola',
-        preparo : 'Coloque o molho sobre a massa, adicione o queijo e espalhe os outros ingredientes por cima do queijo. Leve ao forno por 10 minutos.',
-        img : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
-        $scope.prato2 = {
-        titulo : 'Pizza Mussarela',
-        ingredientes : 'Mussarela e molho',
-        preparo : 'Coloque o molho sobre a massa, adicione o queijo e leve ao forno por 10 minutos.',
-        img : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
+    $scope.pratos = [
+        {
+            titulo: 'Pizza Calabresa',
+            ingredientes: 'Mussarela, calabresa, molho e cebola',
+            preparo: 'Coloque o molho sobre a massa, adicione o queijo e espalhe os outros ingredientes por cima do queijo. Leve ao forno por 10 minutos.',
+            img: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+        },
+        {
+            titulo: 'Pizza Mussarela',
+            ingredientes: 'Mussarela e molho',
+            preparo: 'Coloque o molho sobre a massa, adicione o queijo e leve ao forno por 10 minutos.',
+            img: 'http://www.fundosanimais.com/Minis/lobo-branco.jpg'
+        },
+        {
+            titulo: 'Arroz à Portuense',
+            ingredientes: 'Arroz, alho, carne, molho de tomate, extrato de tomate, cerveja, amaciante de carne, calabresa, cebola, parmesão ralado, requeijão e salsinha' ,
+            preparo: 'Refogue o alho e prepare o arroz, coloque a carne na panela de pressão junto com a cerveja, o molho e extrato de tomate e amaciante de carne e deixei cozinhar por 30min, refogue a cebola e frite a calabresa, depois é so juntar tudo e bom apetit.',
+            img: 'http://www.fundosanimais.com/Minis/pantera-negra.jpg'
+        },
 
-    });
+    ];
+
+
+});
