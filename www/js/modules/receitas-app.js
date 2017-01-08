bd146857b88280585656ed90ef759bea883a11c0
Rodrigo Filomeno <rodrigofilomeno@Mac-mini-de-Rodrigo.local>
2017-01-08 10:26:10 -0200

    inclui pagina que para adicionar itens na lista

--- www/js/modules/receitas-app.js
+++ www/js/modules/receitas-app.js
@@ -1,9 +1,14 @@
 angular.module('ReceitasApp', ['ngRoute', 'minhasDiretivas'])
-.config(function ($routeProvider) {
+    .config(function ($routeProvider) {
         $routeProvider.when('/', {
             templateUrl: 'home.html',
             reloadOnSearch: false
-        })})
+        });
+        $routeProvider.when('/adicionar', {
+            templateUrl: 'adicionar.html',
+            reloadOnSearch: false
+        });
+    })
     .run(['DbFactory', function (DbFactory) {
         DbFactory.loadDb();
     }]);
