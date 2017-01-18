6b047701886a70f67489f2a298d4d7691b43060d
Rodrigo Filomeno <Rodrigo Filomeno>
2017-01-18 18:19:16 -0200

    limpa formulario apos salvar

--- www/js/btnSalvar.js
+++ www/js/btnSalvar.js
@@ -1,3 +1,11 @@
+function limpaForm() {
+    $("#first_name2").val("");
+    $("#textarea1").val('');
+    $("#textarea2").val('');
+
+}
+
+
 $("#salvar").click(function () {
     var item = [{
         nome: $("#first_name2").val(),
@@ -10,10 +18,12 @@ $("#salvar").click(function () {
         createprato(item);
         populalista(item);
         Materialize.toast("Prato adicionado com sucesso!", 1000);
+        limpaForm();
     } else if ($("#tipoReceita").val() == "2") {
         createdoces(item);
         populalistadoces(item);
         Materialize.toast("Doce adicionado com sucesso!", 1000);
+        limpaForm();
     } else {
         Materialize.toast("Escolha o tipo de receita!", 2000, 'rounded');
     }
