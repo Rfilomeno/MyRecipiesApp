b5991adc9830280ca9c32d2a78b03a8f766a80c7
Rodrigo Filomeno <rodrigofilomeno@Mac-mini-de-Rodrigo.local>
2017-01-18 07:12:48 -0200

    grrr faltou tirar do homectrl

--- www/js/controllers/home-controller.js
+++ www/js/controllers/home-controller.js
@@ -3,11 +3,5 @@ angular.module('ReceitasApp').controller('HomeController', ['$scope', 'DbFactory
 
     $scope.pratosList = DbFactory.pratos;
     $scope.docesList = DbFactory.doces;
-    $scope.bebidasList = DbFactory.bebidas;
-
-    console.log(DbFactory.bebidas);
-
-
-
 
 }]);
